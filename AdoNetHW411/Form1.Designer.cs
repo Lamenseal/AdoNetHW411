0299f0e37c2861562166b90db1243341b7801fcb
銘鴻 徐 <kartd0079818@gmail.com>
2022-04-12 15:23:18 +0800

    加入專案檔案。

---
+++ AdoNetHW411/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace AdoNetHW411
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.northWindCategories1 = new AdoNetHW411.NorthWindCategories();
+            this.categoriesTableAdapter1 = new AdoNetHW411.NorthWindCategoriesTableAdapters.CategoriesTableAdapter();
+            this.productsTableAdapter1 = new AdoNetHW411.NorthWindCategoriesTableAdapters.ProductsTableAdapter();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.northWindCategories1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Categories:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(190, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // northWindCategories1
+            // 
+            this.northWindCategories1.DataSetName = "NorthWindCategories";
+            this.northWindCategories1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // categoriesTableAdapter1
+            // 
+            this.categoriesTableAdapter1.ClearBeforeFill = true;
+            // 
+            // productsTableAdapter1
+            // 
+            this.productsTableAdapter1.ClearBeforeFill = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(191, 125);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(443, 280);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(95, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Products";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.northWindCategories1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private NorthWindCategories northWindCategories1;
+        private NorthWindCategoriesTableAdapters.CategoriesTableAdapter categoriesTableAdapter1;
+        private NorthWindCategoriesTableAdapters.ProductsTableAdapter productsTableAdapter1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AdoNetHW411/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AdoNetHW411
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnn = null;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            try
+            {
+                cnn = new SqlConnection("Data Source =.; Initial Catalog = Northwind; Integrated Security = True");
+                cnn.Open();
+                SqlCommand com = new SqlCommand("Select CategoryName from Categories", cnn);
+                SqlDataReader dataReader = com.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    string s = dataReader["CategoryName"].ToString();
+                    //listBox1.Items.Add(dataReader["ProductName"]+" "+ dataReader["UnitPrice"]);
+                    comboBox1.Items.Add(s);
+                }
+
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (cnn != null)
+                {
+                    cnn.Close();
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sli = comboBox1.SelectedItem.ToString();
+            listBox1.Items.Clear();
+            try
+            {
+                cnn = new SqlConnection("Data Source =.; Initial Catalog = Northwind; Integrated Security = True");
+                cnn.Open();
+                SqlCommand com = new SqlCommand(("Select * from Products  p join Categories c on p.CategoryID = c.CategoryID where CategoryName=" + "'"+sli+"'"), cnn);
+                SqlDataReader dataReader = com.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    string s = $"{dataReader["ProductID"].ToString()}    {dataReader["ProductName"].ToString()}   {dataReader["CategoryName"].ToString()}";
+                    //listBox1.Items.Add(dataReader["ProductName"]+" "+ dataReader["UnitPrice"]);
+                    listBox1.Items.Add(s);
+                }
+
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (cnn != null)
+                {
+                    cnn.Close();
+                }
+            }
+
+        }
+    }
+}
